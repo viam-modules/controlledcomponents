44c402ff821d0fa015345b9c8d34fde88b637faf
John <john@viam.com>
2025-04-08 16:23:25 -0400

    add sensor base

---
+++ sensorcontrolled.go
@@ -0,0 +1,391 @@
+// Package controlledcomponents base implements a base with feedback control from a movement sensor
+package controlledcomponents
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/golang/geo/r3"
+	"github.com/pkg/errors"
+
+	"go.viam.com/rdk/components/base"
+	"go.viam.com/rdk/components/movementsensor"
+	"go.viam.com/rdk/control"
+	"go.viam.com/rdk/logging"
+	"go.viam.com/rdk/operation"
+	"go.viam.com/rdk/resource"
+	"go.viam.com/rdk/spatialmath"
+	rdkutils "go.viam.com/rdk/utils"
+)
+
+const (
+	yawPollTime        = 5 * time.Millisecond
+	velocitiesPollTime = 5 * time.Millisecond
+	sensorDebug        = false
+	typeLinVel         = "linear_velocity"
+	typeAngVel         = "angular_velocity"
+	defaultControlFreq = 10 // Hz
+	getPID             = "get_tuned_pid"
+)
+
+var (
+	family = resource.NewModelFamily("viam", "controlled-components")
+	// SensorControlledModel is the name of the sensor_controlled model of a base component.
+	SensorControlledModel = family.WithModel("sensor-controlled")
+	errNoGoodSensor       = errors.New("no appropriate sensor for orientation or velocity feedback")
+)
+
+// Config configures a sensor controlled base.
+type Config struct {
+	MovementSensor    []string            `json:"movement_sensor"`
+	Base              string              `json:"base"`
+	ControlParameters []control.PIDConfig `json:"control_parameters,omitempty"`
+	ControlFreq       float64             `json:"control_frequency_hz,omitempty"`
+}
+
+// Validate validates all parts of the sensor controlled base config.
+func (cfg *Config) Validate(path string) ([]string, error) {
+	deps := []string{}
+	if len(cfg.MovementSensor) == 0 {
+		return nil, resource.NewConfigValidationError(path, errors.New("need at least one movement sensor for base"))
+	}
+	deps = append(deps, cfg.MovementSensor...)
+
+	if cfg.Base == "" {
+		return nil, resource.NewConfigValidationFieldRequiredError(path, "base")
+	}
+	deps = append(deps, cfg.Base)
+
+	for _, pidConf := range cfg.ControlParameters {
+		if pidConf.Type != typeLinVel && pidConf.Type != typeAngVel {
+			return nil, resource.NewConfigValidationError(path,
+				errors.New("control_parameters type must be 'linear_velocity' or 'angular_velocity'"))
+		}
+	}
+
+	return deps, nil
+}
+
+type sensorBase struct {
+	name   resource.Name
+	conf   *Config
+	logger logging.Logger
+	mu     sync.Mutex
+
+	activeBackgroundWorkers sync.WaitGroup
+	controlledBase          base.Base // the inherited wheeled base
+
+	opMgr *operation.SingleOperationManager
+
+	allSensors []movementsensor.MovementSensor
+	velocities movementsensor.MovementSensor
+	position   movementsensor.MovementSensor
+	// headingFunc returns the current angle between (-180,180) and whether Spin is supported
+	headingFunc func(ctx context.Context) (float64, bool, error)
+
+	controlLoopConfig *control.Config
+	blockNames        map[string][]string
+	loop              *control.Loop
+	configPIDVals     []control.PIDConfig
+	tunedVals         *[]control.PIDConfig
+	controlFreq       float64
+}
+
+func (s *sensorBase) Name() resource.Name {
+	return s.name
+}
+
+func init() {
+	resource.RegisterComponent(
+		base.API,
+		SensorControlledModel,
+		resource.Registration[base.Base, *Config]{Constructor: newControlledComponentsSensorControlled})
+}
+
+func newControlledComponentsSensorControlled(ctx context.Context, deps resource.Dependencies, rawConf resource.Config, logger logging.Logger) (base.Base, error) {
+	conf, err := resource.NativeConfig[*Config](rawConf)
+	if err != nil {
+		return nil, err
+	}
+
+	return NewSensorControlled(ctx, deps, rawConf.ResourceName(), conf, logger)
+
+}
+
+func NewSensorControlled(ctx context.Context, deps resource.Dependencies, name resource.Name, conf *Config, logger logging.Logger) (base.Base, error) {
+	sb := &sensorBase{
+		logger:        logger,
+		tunedVals:     &[]control.PIDConfig{{}, {}},
+		configPIDVals: []control.PIDConfig{{}, {}},
+		name:          name,
+		opMgr:         operation.NewSingleOperationManager(),
+	}
+
+	if err := sb.reconfigureWithConfig(ctx, deps, conf); err != nil {
+		return nil, err
+	}
+
+	return sb, nil
+}
+
+func (sb *sensorBase) reconfigureWithConfig(ctx context.Context, deps resource.Dependencies, newConf *Config) error {
+	var err error
+	if sb.loop != nil {
+		sb.loop.Stop()
+		sb.loop = nil
+	}
+
+	sb.mu.Lock()
+	defer sb.mu.Unlock()
+
+	sb.controlFreq = defaultControlFreq
+	if newConf.ControlFreq != 0 {
+		sb.controlFreq = newConf.ControlFreq
+	}
+
+	// reset all sensors
+	sb.allSensors = nil
+	sb.velocities = nil
+	var orientation movementsensor.MovementSensor
+	var compassHeading movementsensor.MovementSensor
+	sb.position = nil
+	sb.controlledBase = nil
+
+	for _, name := range newConf.MovementSensor {
+		ms, err := movementsensor.FromDependencies(deps, name)
+		if err != nil {
+			return errors.Wrapf(err, "no movement sensor named (%s)", name)
+		}
+		sb.allSensors = append(sb.allSensors, ms)
+	}
+
+	for _, ms := range sb.allSensors {
+		props, err := ms.Properties(context.Background(), nil)
+		if err == nil && props.OrientationSupported {
+			// return first sensor that does not error that satisfies the properties wanted
+			orientation = ms
+			sb.logger.CInfof(ctx, "using sensor %s as orientation sensor for base", orientation.Name().ShortName())
+			break
+		}
+	}
+
+	for _, ms := range sb.allSensors {
+		props, err := ms.Properties(context.Background(), nil)
+		if err == nil && props.AngularVelocitySupported && props.LinearVelocitySupported {
+			// return first sensor that does not error that satisfies the properties wanted
+			sb.velocities = ms
+			sb.logger.CInfof(ctx, "using sensor %s as velocity sensor for base", sb.velocities.Name().ShortName())
+			break
+		}
+	}
+
+	for _, ms := range sb.allSensors {
+		props, err := ms.Properties(context.Background(), nil)
+		if err == nil && props.PositionSupported {
+			// return first sensor that does not error that satisfies the properties wanted
+			sb.position = ms
+			sb.logger.CInfof(ctx, "using sensor %s as position sensor for base", sb.position.Name().ShortName())
+			break
+		}
+	}
+
+	for _, ms := range sb.allSensors {
+		props, err := ms.Properties(context.Background(), nil)
+		if err == nil && props.CompassHeadingSupported {
+			// return first sensor that does not error that satisfies the properties wanted
+			compassHeading = ms
+			sb.logger.CInfof(ctx, "using sensor %s as compassHeading sensor for base", compassHeading.Name().ShortName())
+			break
+		}
+	}
+	sb.determineHeadingFunc(ctx, orientation, compassHeading)
+
+	if orientation == nil && sb.velocities == nil {
+		return errNoGoodSensor
+	}
+
+	sb.controlledBase, err = base.FromDependencies(deps, newConf.Base)
+	if err != nil {
+		return errors.Wrapf(err, "no base named (%s)", newConf.Base)
+	}
+
+	if sb.velocities != nil && len(newConf.ControlParameters) != 0 {
+		// assign linear and angular PID correctly based on the given type
+		for _, pidConf := range newConf.ControlParameters {
+			switch pidConf.Type {
+			case typeLinVel:
+				// configPIDVals at index 0 is linear
+				sb.configPIDVals[0] = pidConf
+			case typeAngVel:
+				// configPIDVals at index 1 is angular
+				sb.configPIDVals[1] = pidConf
+			default:
+				return fmt.Errorf("control_parameters type '%v' not accepted, type must be 'linear_velocity' or 'angular_velocity'",
+					pidConf.Type)
+			}
+		}
+
+		// unlock the mutex before setting up the control loop so that the motors
+		// are not locked, and can run if any auto-tuning is necessary
+		sb.mu.Unlock()
+		if err := sb.setupControlLoop(sb.configPIDVals[0], sb.configPIDVals[1]); err != nil {
+			sb.mu.Lock()
+			return err
+		}
+		// relock the mutex after setting up the control loop since there is still a defer unlock
+		sb.mu.Lock()
+	}
+	sb.conf = newConf
+
+	return nil
+}
+
+func (sb *sensorBase) Reconfigure(ctx context.Context, deps resource.Dependencies, conf resource.Config) error {
+	newConf, err := resource.NativeConfig[*Config](conf)
+	if err != nil {
+		return err
+	}
+
+	return sb.reconfigureWithConfig(ctx, deps, newConf)
+}
+
+func (sb *sensorBase) SetPower(
+	ctx context.Context, linear, angular r3.Vector, extra map[string]interface{},
+) error {
+	sb.opMgr.CancelRunning(ctx)
+	if sb.loop != nil {
+		sb.loop.Pause()
+	}
+	return sb.controlledBase.SetPower(ctx, linear, angular, extra)
+}
+
+func (sb *sensorBase) Stop(ctx context.Context, extra map[string]interface{}) error {
+	sb.opMgr.CancelRunning(ctx)
+	if sb.loop != nil {
+		sb.loop.Pause()
+		// update pid controllers to be an at rest state
+		if err := sb.updateControlConfig(ctx, 0, 0); err != nil {
+			return err
+		}
+	}
+	return sb.controlledBase.Stop(ctx, extra)
+}
+
+func (sb *sensorBase) IsMoving(ctx context.Context) (bool, error) {
+	return sb.controlledBase.IsMoving(ctx)
+}
+
+func (sb *sensorBase) Properties(ctx context.Context, extra map[string]interface{}) (base.Properties, error) {
+	return sb.controlledBase.Properties(ctx, extra)
+}
+
+func (sb *sensorBase) Geometries(ctx context.Context, extra map[string]interface{}) ([]spatialmath.Geometry, error) {
+	return sb.controlledBase.Geometries(ctx, extra)
+}
+
+func (sb *sensorBase) DoCommand(ctx context.Context, req map[string]interface{}) (map[string]interface{}, error) {
+	resp := make(map[string]interface{})
+
+	sb.mu.Lock()
+	defer sb.mu.Unlock()
+	ok := req[getPID].(bool)
+	if ok {
+		var respStr string
+		for _, pidConf := range *sb.tunedVals {
+			if !pidConf.NeedsAutoTuning() {
+				respStr += pidConf.String()
+			}
+		}
+		resp[getPID] = respStr
+	}
+
+	return resp, nil
+}
+
+func (sb *sensorBase) Close(ctx context.Context) error {
+	if err := sb.Stop(ctx, nil); err != nil {
+		return err
+	}
+	if sb.loop != nil {
+		sb.loop.Stop()
+		sb.loop = nil
+	}
+
+	sb.activeBackgroundWorkers.Wait()
+	return nil
+}
+
+// determineHeadingFunc determines which movement sensor endpoint should be used for control.
+// The priority is Orientation -> Heading -> No heading control.
+func (sb *sensorBase) determineHeadingFunc(ctx context.Context,
+	orientation, compassHeading movementsensor.MovementSensor,
+) {
+	switch {
+	case orientation != nil:
+
+		sb.logger.CInfof(ctx, "using sensor %s as angular heading sensor for base %v", orientation.Name().ShortName(), sb.Name().ShortName())
+
+		sb.headingFunc = func(ctx context.Context) (float64, bool, error) {
+			orient, err := orientation.Orientation(ctx, nil)
+			if err != nil {
+				return 0, false, err
+			}
+			// this returns (-180-> 180)
+			yaw := rdkutils.RadToDeg(orient.EulerAngles().Yaw)
+
+			return yaw, true, nil
+		}
+	case compassHeading != nil:
+		sb.logger.CInfof(ctx, "using sensor %s as angular heading sensor for base %v", compassHeading.Name().ShortName(), sb.Name().ShortName())
+
+		sb.headingFunc = func(ctx context.Context) (float64, bool, error) {
+			compass, err := compassHeading.CompassHeading(ctx, nil)
+			if err != nil {
+				return 0, false, err
+			}
+			// flip compass heading to be CCW/Z up
+			compass = 360 - compass
+
+			// make the compass heading (-180->180)
+			if compass > 180 {
+				compass -= 360
+			}
+
+			return compass, true, nil
+		}
+	default:
+		sb.logger.CInfof(ctx, "base %v cannot control heading, no heading related sensor given",
+			sb.Name().ShortName())
+		sb.headingFunc = func(ctx context.Context) (float64, bool, error) {
+			return 0, false, nil
+		}
+	}
+}
+
+// if loop is tuning, return an error
+// if loop has been tuned but the values haven't been added to the config, error with tuned values.
+func (sb *sensorBase) checkTuningStatus() error {
+	done := true
+	needsTuning := false
+
+	for i := range sb.configPIDVals {
+		// check if the current signal needed tuning
+		if sb.configPIDVals[i].NeedsAutoTuning() {
+			// return true if either signal needed tuning
+			needsTuning = needsTuning || true
+			// if the tunedVals have not been updated, then tuning is still in progress
+			done = done && !(*sb.tunedVals)[i].NeedsAutoTuning()
+		}
+	}
+
+	if needsTuning {
+		if done {
+			return control.TunedPIDErr(sb.Name().ShortName(), *sb.tunedVals)
+		}
+		return control.TuningInProgressErr(sb.Name().ShortName())
+	}
+
+	return nil
+}
---
+++ spin.go
@@ -0,0 +1,175 @@
+package controlledcomponents
+
+import (
+	"context"
+	"errors"
+	"math"
+	"time"
+)
+
+const (
+	increment        = 0.01 // angle fraction multiplier to check
+	oneTurn          = 360.0
+	maxSlowDownAng   = 30. // maximum angle from goal for spin to begin breaking
+	slowDownAngGain  = 0.1 // Use the final 10% of the requested spin to slow down
+	boundCheckTarget = 1.  // error threshold for spin
+)
+
+// Spin commands a base to turn about its center at an angular speed and for a specific angle.
+// When controls are enabled, Spin polls the provided orientation movement sensor and corrects
+// any error between the desired degsPerSec and the actual degsPerSec using a PID control loop.
+// Spin also monitors the angleDeg and stops the base when the goal angle is reached.
+func (sb *sensorBase) Spin(ctx context.Context, angleDeg, degsPerSec float64, extra map[string]interface{}) error {
+	sb.opMgr.CancelRunning(ctx)
+	ctx, done := sb.opMgr.New(ctx)
+	defer done()
+
+	// If an orientation movement sensor or controls are not configured, we cannot use this Spin method.
+	// Instead we need to use the Spin method of the base that the sensorBase wraps.
+	// If there is no valid velocity sensor, there won't be a controlLoopConfig.
+	if sb.controlLoopConfig == nil {
+		sb.logger.CWarnf(ctx, "control parameters not configured, using %v's Spin method", sb.controlledBase.Name().ShortName())
+		return sb.controlledBase.Spin(ctx, angleDeg, degsPerSec, extra)
+	}
+
+	// check tuning status
+	if err := sb.checkTuningStatus(); err != nil {
+		return err
+	}
+
+	prevAngle, hasOrientation, err := sb.headingFunc(ctx)
+	if err != nil {
+		return err
+	}
+
+	if !hasOrientation {
+		sb.logger.CWarn(ctx,
+			"controlling using angular velocity only, for increased accuracy add an orientation or compass heading reporting sensor")
+	}
+
+	// make sure the control loop is enabled
+	if sb.loop == nil {
+		if err := sb.startControlLoop(); err != nil {
+			return err
+		}
+	}
+
+	// pause and resume the loop to reset the control blocks
+	// This prevents any residual signals in the control loop from "kicking" the robot
+	sb.loop.Pause()
+	sb.loop.Resume()
+	var angErr, angMoved float64
+
+	// to keep the signs simple, ensure degsPerSec is positive and let angleDeg handle the direction of the spin
+	if degsPerSec < 0 {
+		angleDeg = -angleDeg
+		degsPerSec = -degsPerSec
+	}
+	slowDownAng := calcSlowDownAng(angleDeg)
+
+	ticker := time.NewTicker(time.Duration(1000./sb.controlLoopConfig.Frequency) * time.Millisecond)
+	defer ticker.Stop()
+
+	// timeout duration is a multiplier times the expected time to perform a movement
+	spinTimeEst := time.Duration(int(time.Second) * int(math.Abs(angleDeg/degsPerSec)))
+	startTime := time.Now()
+	timeOut := 5 * spinTimeEst
+	if timeOut < 10*time.Second {
+		timeOut = 10 * time.Second
+	}
+	prevTime := startTime
+
+	for {
+		if err := ctx.Err(); err != nil {
+			ticker.Stop()
+			return err
+		}
+
+		select {
+		case <-ctx.Done():
+			// context.cancelled can happen due to UI being closed during Spin.
+			// Do not return context canceled errors, just log them
+			if errors.Is(ctx.Err(), context.Canceled) {
+				sb.logger.Warn("Context cancelled during Spin", ctx.Err())
+				return nil
+			}
+			return err
+		case <-ticker.C:
+
+			if hasOrientation {
+				currYaw, _, err := sb.headingFunc(ctx)
+				if err != nil {
+					return err
+				}
+				// use initial angle to get the current angle the spin has moved
+				angMoved = getMovedAng(prevAngle, currYaw, angMoved)
+
+				// track the previous angle to compute how much we moved with each iteration
+				prevAngle = currYaw
+			} else {
+				currTime := time.Now()
+				angVels, err := sb.velocities.AngularVelocity(ctx, nil)
+				if err != nil {
+					return err
+				}
+				deltaTime := currTime.Sub(prevTime).Seconds()
+				// calculate the estimated change in angle based on the latest angular velocity
+				deltaAngDeg := angVels.Z * deltaTime
+				angMoved += deltaAngDeg
+
+				// track time for the velocity integration
+				prevTime = currTime
+			}
+
+			// compute the error
+			angErr = (angleDeg - angMoved)
+
+			if math.Abs(angErr) < boundCheckTarget {
+				return sb.Stop(ctx, nil)
+			}
+			angVel := calcAngVel(angErr, degsPerSec, slowDownAng)
+
+			if err := sb.updateControlConfig(ctx, 0, angVel); err != nil {
+				return err
+			}
+
+			// check if the duration of the spin exceeds the expected length of the spin
+			if time.Since(startTime) > timeOut {
+				sb.logger.CWarn(ctx, "exceeded time for Spin call, stopping base")
+				return sb.Stop(ctx, nil)
+			}
+		}
+	}
+}
+
+// calcSlowDownAng computes the angle at which the spin should begin to slow down.
+// This helps to prevent overshoot when reaching the goal and reduces the jerk on the robot when the spin is complete.
+// This term should always be positive.
+func calcSlowDownAng(angleDeg float64) float64 {
+	return math.Min(math.Abs(angleDeg)*slowDownAngGain, maxSlowDownAng)
+}
+
+// calcAngVel computes the desired angular velocity based on how far the base is from reaching the goal.
+func calcAngVel(angErr, degsPerSec, slowDownAng float64) float64 {
+	// have the velocity slow down when appoaching the goal. Otherwise use the desired velocity
+	angVel := angErr * degsPerSec / slowDownAng
+	if math.Abs(angVel) > degsPerSec {
+		return degsPerSec * sign(angVel)
+	}
+	return angVel
+}
+
+// getMovedAng tracks how much the angle has moved between each sensor update.
+// This allows us to convert a bounded angle(0 to 360 or -180 to 180) into the raw angle traveled.
+func getMovedAng(prevAngle, currAngle, angMoved float64) float64 {
+	// the angle changed from 180 to -180. this means we are spinning in the negative direction
+	if currAngle-prevAngle < -300 {
+		return angMoved + currAngle - prevAngle + 360
+	}
+	// the angle changed from -180 to 180
+	if currAngle-prevAngle > 300 {
+		return angMoved + currAngle - prevAngle - 360
+	}
+	// add the change in angle to the position
+	return angMoved + currAngle - prevAngle
+}
